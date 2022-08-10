74703723998f183d7eca2daf4a9e210251a363bc
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-08-10 13:24:27 +0500

    Update AlphabeticCharacters.java

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -1,6 +1,46 @@
 package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
-    public void vowelDeterminer(char character) {
+    public static void vowelDeterminer(char character) {
+        switch (character) {
+            case 'a':
+            case 'o':
+            case 'i':
+            case 'e':
+            case 'u':
+            case 'y':
+                System.out.println("Vowel");
+                break;
+            case 'b':
+            case 'c':
+            case 'd':
+            case 'f':
+            case 'g':
+            case 'h':
+            case 'j':
+            case 'k':
+            case 'l':
+            case 'm':
+            case 'n':
+            case 'p':
+            case 'q':
+            case 'r':
+            case 's':
+            case 't':
+            case 'v':
+            case 'w':
+            case 'x':
+            case 'z':
+                System.out.println("Consonant");
+                break;
+        
+        default: {
+            System.out.println("wrong alphabet!");
+        }
+        
+        }
+    }
+    public static void main(String[] args) {
+        vowelDeterminer('a');
     }
 }
