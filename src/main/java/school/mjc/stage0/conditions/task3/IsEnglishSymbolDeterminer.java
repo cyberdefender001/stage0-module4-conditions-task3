902de526d21e872a45ae8ff74c262c867a414756
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-08-10 14:31:30 +0500

    Update IsEnglishSymbolDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -1,6 +1,44 @@
 package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
-    public void isEnglishSymbol(char symbol) {
+    public static void vowelDeterminer(char character) {
+        switch (character) {
+            case 'a':
+            case 'o':
+            case 'i':
+            case 'e':
+            case 'u':
+            case 'y':
+            case 'b':
+            case 'c':
+            case 'd':
+            case 'f':
+            case 'g':
+            case 'h':
+            case 'j':
+            case 'k':
+            case 'l':
+            case 'm':
+            case 'n':
+            case 'p':
+            case 'q':
+            case 'r':
+            case 's':
+            case 't':
+            case 'v':
+            case 'w':
+            case 'x':
+            case 'z':
+                System.out.println("English");
+                break;
+        
+        default: {
+            System.out.println("Non English");
+        }
+        
+        }
+    }
+    public static void main(String[] args) {
+        vowelDeterminer('a');
     }
 }
