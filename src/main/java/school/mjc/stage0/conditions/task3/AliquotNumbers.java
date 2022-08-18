3f0e4968fef933dc34e34e7f8167a2ef536b34a3
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-08-18 11:15:49 +0500

    Update AliquotNumbers.java

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -1,6 +1,23 @@
 package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
-    public void isFirstAliquot(int first, int second) {
+    public static void isFirstAliquot(int first, int second) {
+        int sum = 0;
+        for(int i = 1; i < first; i++) {
+            if (first % i == 0) {
+                sum += i;
+            }
+        }
+        if (sum == second) {
+            System.out.println ("Aliquot");
+        } else if (sum == 0) {
+            System.out.println("Not aliquot");
+        } else {
+            isFirstAliquot(sum,second);
+        }
+        
+    }
+    public static void main(String[] args) {
+        isFirstAliquot(12,4);
     }
 }
