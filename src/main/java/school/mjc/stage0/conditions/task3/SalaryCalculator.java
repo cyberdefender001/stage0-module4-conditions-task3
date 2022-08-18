d244c7518e3ff6105a2e1a78cd2635c33841a25d
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-08-18 10:38:23 +0500

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -1,6 +1,22 @@
 package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
-    public void calculateSalary(int salary) {
+    public static void calculateSalary(int salary) {
+        int taxes = 0;
+        if (salary == 0) {
+            System.out.println("wrong input!");
+        }else if (salary <= 10000) {
+            salary = salary - salary * 0.15;
+            System.out.println(salary);
+        } else if (salary > 10000 && salary <= 20000) {
+            salary = salary - salary * 0.18;
+            System.out.println(salary);
+        } else if (salary > 20000) {
+            salary = salary - salary * 0.2;
+            System.out.println(salary);
+        } 
+    }
+    public static void main(String[] args) {
+        calculateSalary(15000);
     }
 }
