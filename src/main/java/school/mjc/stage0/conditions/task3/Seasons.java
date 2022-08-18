ee1d6ea56df5e44fe4b62e5ed1bed015391bab02
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-08-18 11:19:55 +0500

    Update Seasons.java

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -7,22 +7,27 @@ public class Seasons {
             case 1:
             case 2:
                 System.out.println("Winter");
+                break;
             case 3:
             case 4:
             case 5:
                 System.out.println("Spring");
+                break;
             case 6:
             case 7:
             case 8:
                 System.out.println("Summer");
+                break;
             case 9:
             case 10:
             case 11:
                 System.out.println("Autumn");
-        }
+                break;
         default: {
             System.out.println("wrong number!");
         }
+        }
+        
     }
     public static void main(String[] args) {
         tellTheSeasonByMonthNumber(6);
